51e540dfd9a7b19cac69e67f339a8911a053ca53
vhdev <30373406+VladislavHacker@users.noreply.github.com>
2023-05-17 17:34:59 +0300

    Update run.sh

--- task0/run.sh
+++ task0/run.sh
@@ -2,4 +2,4 @@
 
 silution_out=$1 
 correct_out=$2
-diff -s --strip-trailing-cr $silution_out $correct_out > /dev/null && echo 'OK' || echo 'WA'"
+diff -s --strip-trailing-cr $silution_out $correct_out > /dev/null && echo 'OK' || echo 'WA'
